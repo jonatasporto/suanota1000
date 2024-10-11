ed623492a6451b31adb182dad1b26ca7ea75a666
Jonatas Porto Mendes <jonatasporto@outlook.com>
2024-10-11 15:14:55 -0300

    4º commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
     <html lang="Pt-Br">
       <GoogleTagManage />
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}      
+        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}      
       >
         {children}
       </body>
